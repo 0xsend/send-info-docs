46b58aead415b56d215a0a83b870650cf077f3f1
ethentree <ethen@send.it>
2026-01-29 11:40:57 -0800

    update google icons

--- app/layout.jsx
+++ app/layout.jsx
@@ -9,6 +9,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+      </head>
       <body>
         {children}
       </body>
